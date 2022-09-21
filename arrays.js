afc71705883fabe4a3f660455141ca0614d94e77
Sarah Hampton <stoluba@gmail.com>
2022-09-21 19:43:53 -0400

    Add finishing touches

--- arrays.js
+++ arrays.js
@@ -16,3 +16,8 @@ console.log(ages)
 isOnlineStatuses.unshift(false)
 
 console.log(isOnlineStatuses)
+
+//using ages array as an example, the index would be the counting of the elements.
+// index value of 36 is 0 in the begining.
+console.log(ages[0])
+//after using the shift method 45 is now in the 0 index position.
