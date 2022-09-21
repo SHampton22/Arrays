d828aa70a8f4e17cb29db9a8684fad86d2858535
Sarah Hampton <stoluba@gmail.com>
2022-09-21 19:38:15 -0400

    Add descriptions

--- arrays.js
+++ arrays.js
@@ -1,3 +1,18 @@
 var friends = ["sarah", "cait", "missy", "ashley"]
 var ages = [36, 45, 50, 38]
 var isOnlineStatuses = [true, true, false, false]
+
+//push will add elements to the end of the array.
+friends.push("lindsey")
+
+console.log(friends)
+
+//shift removes the first element of an array.
+ages.shift()
+
+console.log(ages)
+
+//unshift adds an element to the end of an array.
+isOnlineStatuses.unshift(false)
+
+console.log(isOnlineStatuses)
